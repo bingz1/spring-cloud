1d0a128057e221b85c03d1b9956b739e30f1c1e2
zxb <admin@example.com>
2016-07-19 17:24:04 +0800

    feature:增加 spring cloud config

---
+++ spring-config-client/src/main/java/cloud/simple/config/ConfigClientApplication.java
@@ -0,0 +1,32 @@
+package cloud.simple.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zxb on 7/18/16.
+ */
+@SpringBootApplication
+@RestController
+@Configuration
+@EnableAutoConfiguration
+public class ConfigClientApplication {
+
+    @Value("${spring.application.name}")
+    String name = "World";
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
--- spring-eureka/src/test/java/cloud/simple/eureka/EurekaServerApplicationTest.java
+++ spring-eureka/src/test/java/cloud/simple/eureka/EurekaServerApplicationTest.java
@@ -1,15 +1,41 @@
 package cloud.simple.eureka;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by zxb on 7/5/16.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EurekaServerApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port=0")
 public class EurekaServerApplicationTest {
 
-    @org.junit.Test
-    public void main() throws Exception {
+    @Value("${local.server.port}")
+    private int port = 0;
 
+    @Test
+    public void catalogLoads(){
+        ResponseEntity<Map> entity = new TestRestTemplate().getForEntity("http://localhost:"+port+"eureka/apps",Map.class);
+        assertEquals(HttpStatus.OK,entity.getStatusCode());
     }
 
+    @Test
+    public void adminLoads(){
+        ResponseEntity<Map> entity = new TestRestTemplate().getForEntity("http://localhost:"+port+"/env",Map.class);
+        assertEquals(HttpStatus.OK,entity.getStatusCode());
+    }
 }
